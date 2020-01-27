3cb1b898c7b3495daa52c9fb80214e675b86b47c
Aditya Kamath <adi.k2405@gmail.com>
2020-01-27 14:50:42 +0100

    Create GeometricDistributionI.cpp

---
+++ GeometricDistributionI.cpp
@@ -0,0 +1,27 @@
+//https://www.hackerrank.com/challenges/s10-geometric-distribution-1/tutorial
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+float geometric(int n, float p)
+{
+    return pow((1-p),(n-1))*p;
+}
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    float num, den;
+    cin >> num >> den;
+    float prob = num/den;
+
+    int numInsp;
+    cin >> numInsp;
+
+    cout.precision(3);
+    cout << fixed << geometric(numInsp, prob) << endl; 
+    return 0;
+}
